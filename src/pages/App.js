35e4a59bc25eb09a082f5be96c1bccdb7ef427e3
MarcosWinther <winthermarcos@gmail.com>
2023-11-24 11:22:45 -0300

    Criado o componente de botão buscar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,19 @@
 
+import { useState } from 'react';
 import gitLogo from '../assets/github.png';
 import Input from '../components/Input';
+import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 
 import { Container } from './styles';
 
 function App() {
+  const [repos, setRepos] = useState([]);
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="logo GitHub" />
       <Input />
+      <Button />
       <ItemRepo />
     </Container>
   );
